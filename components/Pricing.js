9f5099636ccd354e3d351f2b283f4399624437e4
dbotti <davide.botti@lastminute.com>
2024-02-23 19:04:37 +0100

    Project setup

---
+++ components/Pricing.js
@@ -0,0 +1,153 @@
+import React, { useMemo } from "react";
+import Image from "next/image";
+import Testimoni from "./Testimoni";
+import ButtonPrimary from "./misc/ButtonPrimary";
+import ButtonOutline from "./misc/ButtonOutline.";
+import Maps from "../public/assets/HugeGlobal.svg";
+import { motion } from "framer-motion";
+import getScrollAnimation from "../utils/getScrollAnimation";
+import ScrollAnimationWrapper from "./Layout/ScrollAnimationWrapper";
+import { CALLINK } from "./Feature";
+
+const Pricing = () => {
+  const scrollAnimation = useMemo(() => getScrollAnimation(), []);
+
+  return (
+    <div
+      className="bg-gradient-to-b from-white-300 to-white-500 w-full py-14"
+      id="pricing"
+    >
+      <div className="max-w-screen-xl  px-6 sm:px-8 lg:px-16 mx-auto flex flex-col w-full text-center justify-center">
+        <div className="flex flex-col w-full">
+          <ScrollAnimationWrapper>
+            <motion.h3
+              variants={scrollAnimation}
+              className="text-2xl sm:text-3xl lg:text-4xl font-medium text-black-600 leading-relaxed"
+            >
+              Tipologie di lezione
+            </motion.h3>
+            <motion.p
+              variants={scrollAnimation}
+              className="leading-normal w-10/12 sm:w-7/12 lg:w-6/12 mx-auto my-2 text-center"
+            >
+              Ecco le tipologie di lezione
+            </motion.p>
+          </ScrollAnimationWrapper>
+          <div className="grid grid-flow-row sm:grid-flow-col grid-cols-1 sm:grid-cols-2 gap-4 lg:gap-12 py-8 lg:py-12 px-6 sm:px-0 lg:px-6">
+            <ScrollAnimationWrapper className="flex justify-center">
+              <motion.div
+                variants={scrollAnimation}
+                className="flex flex-col justify-center items-center border-2 border-gray-500 rounded-xl py-4 px-6 lg:px-12 xl:px-20"
+                whileHover={{
+                  scale : 1.1,
+                  transition: {
+                    duration: .2
+                  }
+                }}
+              >
+                <div className="p-4 lg:p-0 mt-6 lg:mt-16">
+                  <Image
+                    src="/assets/Free.png"
+                    width={145}
+                    height={165}
+                    alt="Free Plan"
+                  />
+                </div>
+                <p className="text-lg text-black-600 font-medium capitalize my-2 sm:my-7">
+                  Singola Online{" "}
+                </p>
+                <ul className="flex flex-col list-inside pl-6 xl:pl-0 items-start justify-start text-left text-black-500 flex-grow">
+                  <li className="relative check custom-list my-2">
+                    Zoom{" "}
+                  </li>
+                  <li className="relative check custom-list my-2">
+                    Discord{" "}
+                  </li>
+                  <li className="relative check custom-list my-2">
+                    Google Meet{" "}
+                  </li>
+                </ul>
+                <div className="flex flex-col w-full justify-center mb-8 flex-none mt-12">
+                  <p className="text-2xl text-black-600 text-center mb-4 ">
+                    CHF 32 <span className="text-black-500">/ ora</span>
+                  </p>
+                  <a href={CALLINK}>
+                    <ButtonOutline>Prenota</ButtonOutline>
+                  </a>
+                </div>
+              </motion.div>
+            </ScrollAnimationWrapper>
+            <ScrollAnimationWrapper className="flex justify-center">
+              <motion.div
+                variants={scrollAnimation}
+                className="flex flex-col justify-center items-center border-2 border-gray-500 rounded-xl py-4 px-6 lg:px-12 xl:px-20"
+                whileHover={{
+                  scale : 1.1,
+                  transition: {
+                    duration: .2
+                  }
+                }}
+              >
+              <div className="p-4 lg:p-0 mt-6 lg:mt-16">
+                <Image
+                  src="/assets/Standard.png"
+                  width={145}
+                  height={165}
+                  alt="Standard Plan"
+                />
+              </div>
+              <p className="text-lg text-black-600 font-medium capitalize my-2 sm:my-7">
+                Lezione di Gruppo Online{" "}
+              </p>
+              <ul className="flex flex-col list-inside pl-6 xl:pl-0 items-start justify-start text-left text-black-500 flex-grow">
+                <li className="relative check custom-list my-2">
+                  Zoom{" "}
+                </li>
+                <li className="relative check custom-list my-2">
+                  Discord{" "}
+                </li>
+                <li className="relative check custom-list my-2">
+                  Google Meet{" "}
+                </li>
+              </ul>
+              <div className="flex flex-col w-full justify-center mb-8 flex-none mt-12">
+                <p className="text-2xl text-black-600 text-center mb-4 ">
+                  CHF 50 <span className="text-black-500">/ ora</span>
+                </p>
+                <a href={CALLINK}>
+                  <ButtonOutline>Prenota</ButtonOutline>
+                </a>
+              </div>
+              </motion.div>
+            </ScrollAnimationWrapper>
+          </div>
+        </div>
+
+        <div className="flex flex-col w-full my-16" id="testimoni">
+          <ScrollAnimationWrapper>
+            <motion.h3
+              variants={scrollAnimation}
+              className="text-2xl sm:text-3xl lg:text-4xl font-medium text-black-600 leading-normal w-9/12 sm: lg:w-4/12 mx-auto">
+              Affidabili, Precisi, Efficienti{" "}
+            </motion.h3>
+            <motion.p
+              variants={scrollAnimation}
+              className="leading-normal mx-auto mb-2 mt-4 w-10/12 sm:w-7/12 lg:w-6/12"
+            >
+              Ritenuti affidabili da tanti studenti soddisfatti (e promossi)
+            </motion.p>
+          </ScrollAnimationWrapper>
+
+          <ScrollAnimationWrapper className="w-full flex flex-col py-12">
+            <motion.div variants={scrollAnimation}>
+              <Testimoni />
+            </motion.div>
+          </ScrollAnimationWrapper>
+          
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Pricing;
